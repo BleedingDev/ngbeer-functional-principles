95a08ccefad22dea23f0ae4a13e9e139f76b202f
Petr Glaser <12586960+pegak@users.noreply.github.com>
2019-06-17 18:02:26 +0200

    Update new-code.ts

--- new-code.ts
+++ new-code.ts
@@ -1,28 +1,18 @@
 this.route.queryParams
   .pipe(
-    takeUntil(this.unsubscribe$),
-    switchMap((currentRoute: { playlistId: string; clipId: string }) => {
-      this.playlist$ = this.store.pipe(select($playlist(currentRoute.playlistId)));
-      this.clips$ = this.store.pipe(select($playlistClips(currentRoute.playlistId)));
-      this.clip$ = this.store.pipe(
-        select($playlistsClip(currentRoute.playlistId, currentRoute.clipId))
-      );
-      return this.clip$;
-    }),
+    switchMap(
+      (route: { playlistId: string; clipId: string }) =>
+        this.store.pipe(select($playlistsClip(route.playlistId, route.clipId)))
+    ),
     filter((clip): clip is Clip => clip !== null),
-    withLatestFrom(this.store.pipe(select($currentVideo))),
-    switchMap(([clip, video]) => {
-      this.currentClip = clip;
-      this.currentVideo = video;
-      return this.store.pipe(select($recordingEntity(this.currentClip.recordingId)));
-    }),
+    switchMap(clip => this.store.pipe(select($recording(clip.recordingId)))),
     filter(recording => recording !== null),
     tap(recording => {
-      this.currentRecording = recording;
-      this.recordingVideos$ = this.store.pipe(select($recordingVideos(recording.id)));
       this.store.dispatch(new SetCurrentVideo(recording.videos));
     }),
     filter(recording => recording.videos.length > 0),
-    switchMap(() => this.store.pipe(select($currentVideo)))
+    switchMapTo(this.store.pipe(select($currentVideo)))
   )
-  .subscribe();
+  .subscribe((currentVideo: Video) => {
+    // Some work with current video
+  });
