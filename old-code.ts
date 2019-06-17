949e56ebd3f3c6adbfe91bedca389cb6fc95e4c9
Petr Glaser <12586960+pegak@users.noreply.github.com>
2019-06-17 18:02:09 +0200

    Update old-code.ts

--- old-code.ts
+++ old-code.ts
@@ -1,32 +1,20 @@
-this.route.queryParams.pipe(takeUntil(this.unsubscribe$)).subscribe((currentRoute: { playlistId: string; clipId: string }) => {
-  this.playlist$ = this.store.pipe(select($playlist(currentRoute.playlistId)));
-  this.clips$ = this.store.pipe(select($playlistAnotations(currentRoute.playlistId)));
-  this.clip$ = this.store.pipe(select($playlistsClip(currentRoute.playlistId, currentRoute.clipId)));
-  this.playlist$.pipe(takeUntil(this.unsubscribe$)).subscribe((playlist: Playlist) => {
-    this.currentPlaylist = playlist;
-  });
+this.route.queryParams.subscribe((route: { playlistId: string; clipId: string }) => {
+  this.clip$ = this.store.pipe(
+    select($playlistsClip(route.playlistId, currentRoute.clipId))
+  );
 
-  this.clip$.pipe(takeUntil(this.unsubscribe$)).subscribe((currentClip: Clip | null) => {
+  this.clip$.subscribe((currentClip: Clip | null) => {
     if (currentClip != null) {
-      this.currentClip = currentClip;
-      this.recording$ = this.store.pipe(select($recordingEntity(currentClip.recordingId)));
+      this.recording$ = this.store.pipe(select($recording(currentClip.recordingId)));
 
-      this.recording$.pipe(takeUntil(this.unsubscribe$)).subscribe((currentRecording: Recording | null) => {
+      this.recording$.subscribe((currentRecording: Recording | null) => {
         if (currentRecording != null) {
-          this.currentRecording = currentRecording;
-          this.recordingVideos$ = this.store.pipe(select($recordingVideos(currentRecording.id)));
           this.store.dispatch(new ChangeCurrentVideo(currentRecording.videos[0]));
 
           if (currentRecording.videos) {
-            this.store
-              .pipe(
-                takeUntil(this.unsubscribe$),
-                select($currentVideo)
-              )
-              .subscribe((currentVideo: Video) => {
-                this.currentVideo = currentVideo;
-                this.calcClipValue = this.calculateTagDuration(currentClip, currentVideo);
-              });
+            this.store.pipe(select($currentVideo)).subscribe((currentVideo: Video) => {
+              // Some work with current video
+            });
           }
         }
       });
